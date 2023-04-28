31e7a77846bbd80c5055917e4a2dd2035993292b
periiProggramerGuy <jonathan.peri56@gmail.com>
2023-04-28 23:34:46 +0300

    more math operations

--- Math.h
+++ Math.h
@@ -1,12 +1,55 @@
-#pragma once
+#ifndef MATH_H
+#define MATH_H
 
-float Deg(float radians)
-{
+class Math {
+public:
+    static float Deg(float radians)
+    {
+        return radians * 180 / 3.14159;
+    }
+    static float Rad(float radians)
+    {
+        return radians;
+    }
+    static float Lerp(float from, float to, float time)
+    {
+        return from + time * (to - from);
+    }
+    static float Power(float base, float exponent)
+    {
+        return pow(base, exponent);
+    }
+    static float Abs(float value)
+    {
+        return value >= 0 ? value : -value;
+    }
+    static float Sqrt(float value)
+    {
+        return sqrt(value);
+    }
+    static float Ceil(float value)
+    {
+        return ceil(value);
+    }
+    static float Floor(float value)
+    {
+        return floor(value);
+    }
+    static float Sin(float radians)
+    {
+        return sin(radians);
+    }
 
-	return radians * 3.14159 / 180;
-}
+    static float Cos(float radians)
+    {
+        return cos(radians);
+    }
 
-float Lerp(float from, float to, float time)
-{
-	return from + time * (to - from);
-}
+    static float Tan(float radians)
+    {
+        return tan(radians);
+    }
+
+};
+
+#endif  // MATH_H
