22f7a3908443b1bddff0a8360a69cabcd425bc68
periiProggramerGuy <jonathan.peri56@gmail.com>
2023-04-29 17:02:11 +0300

    better builder

--- JustAscript.cpp
+++ JustAscript.cpp
@@ -2,7 +2,7 @@
 #include"JustAscript.h"
 void JustAscript::Start(Console& ovcon, std::vector<Object>& sceneObjects)
 {
-    ovcon.log("Welcome!");
+    ovcon.log("TEST6");
 }
 void JustAscript::Update(Console& ovcon, std::vector<Object>& sceneObjects)
 {
--- builder.py
+++ builder.py
@@ -2,36 +2,64 @@ import subprocess
 import shutil
 from pathlib import Path
 import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
-sln_file_path = r'Vision_engine.sln'
+class HotReloadHandler(FileSystemEventHandler):
+    def __init__(self, msbuild_path, sln_file_path, dst_directory_path):
+        super().__init__()
+        self.msbuild_path = msbuild_path
+        self.sln_file_path = sln_file_path
+        self.dst_directory_path = dst_directory_path
+        self.build()
 
-# Set the path to MSBuild.exe and the solution file
-msbuild_path = r"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+    def build(self):
+        print("Building...")
+        result = subprocess.run([self.msbuild_path, self.sln_file_path, '/p:Configuration=Release'])
+        if result.returncode == 0:
+            print("Build succeeded!")
+            self.execute()
+        else:
+            print("Build failed.")
 
-# Build the solution in release mode
-result = subprocess.run([msbuild_path, sln_file_path, '/p:Configuration=Release'])
-if result.returncode == 0:
-    print("Build succeeded!")
-else:
-    print("Build failed.")
+    def execute(self):
+        src_file_path = "x64/Release/Vision_engine.exe"
+
+        if os.path.exists(os.path.join(self.dst_directory_path, "Vision_engine.exe")):
+            os.remove(os.path.join(self.dst_directory_path, "Vision_engine.exe"))
+
+        shutil.move(src_file_path, self.dst_directory_path)
+
+        os.chdir(self.dst_directory_path)
+
+        print("Executing...")
+        os.system('Vision_engine.exe')
+
+    def on_modified(self, event):
+        if event.is_directory or not event.src_path.endswith('.cpp'):
+            return
+        print(f"Detected change in {event.src_path}. Rebuilding...")
+        self.build()
 
 
-# Source file path
-src_file_path = "x64/Release/Vision_engine.exe"
+if __name__ == '__main__':
+    msbuild_path = r"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+    sln_file_path = r'Vision_engine.sln'
 
-# Destination directory path
-dst_directory_path = os.path.dirname(os.path.abspath(__file__))
+    dst_directory_path = os.path.dirname(os.path.abspath(__file__))
 
-# Remove the old file if it exists
-if os.path.exists(os.path.join(dst_directory_path, "Vision_engine.exe")):
-    os.remove(os.path.join(dst_directory_path, "Vision_engine.exe"))
+    handler = HotReloadHandler(msbuild_path, sln_file_path, dst_directory_path)
 
-# Move file to destination directory
-shutil.move(src_file_path, dst_directory_path)
+    observer = Observer()
+    observer.schedule(handler, '.', recursive=True)
+    observer.start()
 
-# Change the working directory to the destination directory
-os.chdir(dst_directory_path)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
 
-# Execute the file
-os.system('Vision_engine.exe')
+    observer.join()
