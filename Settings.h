c9fe3adf07b02f641a50d78b0964854982909a50
periiProggramerGuy <jonathan.peri56@gmail.com>
2023-05-29 14:10:06 +0300

    FXAA

--- Settings.h
+++ Settings.h
@@ -114,18 +114,56 @@ uniform sampler2D screenTexture;
 uniform float radius= 0.2;
 uniform float softness= 0.2;
 
+float FXAA_SPAN_MAX = 8.000f;
+float FXAA_REDUCE_MIN = 1.0/ 128.000f;
+float FXAA_REDUCE_MUL = 1.0/ 8.000f;
+vec2 resolution = vec2(1520, 855);
+vec3 ApplyFXAA() {
+    vec2 inverse_resolution = vec2(1.0 / resolution.x, 1.0 / resolution.y);
+    vec3 rgbNW = texture(screenTexture, texCoords + (vec2(-1.0, -1.0) * inverse_resolution)).rgb;
+    vec3 rgbNE = texture(screenTexture, texCoords + (vec2(1.0, -1.0) * inverse_resolution)).rgb;
+    vec3 rgbSW = texture(screenTexture, texCoords + (vec2(-1.0, 1.0) * inverse_resolution)).rgb;
+    vec3 rgbSE = texture(screenTexture, texCoords + (vec2(1.0, 1.0) * inverse_resolution)).rgb;
+    vec3 rgbM = texture(screenTexture, texCoords).rgb;
+    vec3 luma = vec3(0.299, 0.587, 0.114);
+    float lumaNW = dot(rgbNW, luma);
+    float lumaNE = dot(rgbNE, luma);
+    float lumaSW = dot(rgbSW, luma);
+    float lumaSE = dot(rgbSE, luma);
+    float lumaM = dot(rgbM, luma);
+    float lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));
+    float lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));
+    vec2 dir;
+    dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
+    dir.y = ((lumaNW + lumaSW) - (lumaNE + lumaSE));
+    float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) * 0.25 * FXAA_REDUCE_MUL, FXAA_REDUCE_MIN);
+    float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
+    dir = min(vec2(FXAA_SPAN_MAX, FXAA_SPAN_MAX), max(vec2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX), dir * rcpDirMin)) * inverse_resolution;
+    vec3 rgbA = 0.5 * (
+        texture(screenTexture, texCoords.xy + dir * (1.0 / 3.0 - 0.5)).rgb +
+        texture(screenTexture, texCoords.xy + dir * (2.0 / 3.0 - 0.5)).rgb);
+    vec3 rgbB = rgbA * 0.5 + 0.25 * (
+        texture(screenTexture, texCoords.xy + dir * (-0.5)).rgb +
+        texture(screenTexture, texCoords.xy + dir * (0.5)).rgb);
+    return vec3(rgbB);
+}
 void main()
 {
     vec2 center = vec2(0.5, 0.5);
-    //float radius = 0.7;
-    //float softness = 0.2;
 
     float dist = distance(texCoords, center);
     float vignette = smoothstep(radius, radius - softness, dist);
-
+    
+    vec3 fxaa = ApplyFXAA(); // Assuming ApplyFXAA() is a function that returns vec3 color from FXAA algorithm
+    
     vec4 color = texture(screenTexture, texCoords);
-    FragColor = color * vignette;
+    
+    vec3 finalColor = color.rgb * vignette;
+    finalColor = mix(finalColor, fxaa, vignette); // Apply FXAA effect based on vignette
+    
+    FragColor = vec4(finalColor, 1.0);
 }
+
 )";
 
 
