0b9341ff4f6d9403591868348ac3facfdf3801f2
periiProggramerGuy <jonathan.peri56@gmail.com>
2023-06-18 19:51:14 +0300

    laying foundation for public script variables

--- OV.h
+++ OV.h
@@ -1,6 +1,9 @@
 #ifndef OV_H
 #define OV_H
 #include"Object.h"
+#include <fstream>
+#include <iostream>
+#include <filesystem>
 class OV {
 public:
 	static Object* SearchObjectByName(std::string Name,std::vector<Object>& sceneObjects) {
@@ -17,7 +20,39 @@ public:
 	{
 		obj.tex = Texture(Path.c_str());
 	}
+	
+	static float PublicFloat(int placement, std::string ID) {
+		std::string path = ID.substr(5) + ".ovpmf";
+		if (!std::filesystem::exists(path)) {
+			std::ofstream writer(path);
+			writer << placement;
+		}
 
+		std::ifstream reader(path);
+		std::string line;
+		std::getline(reader, line);
+		placement = std::stof(line);
+
+		return placement;
+	}
+
+
+	
+
+	glm::vec2 getImageAspectRatio(const char* filename) {
+		int width, height, channels;
+		stbi_uc* imageData = stbi_load(filename, &width, &height, &channels, 0);
+
+		if (imageData != nullptr) {
+			float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
+			stbi_image_free(imageData);
+			return glm::vec2(aspectRatio, 1.0f);
+		}
+		else {
+			std::cout << "Failed to load image." << std::endl;
+			return glm::vec2(0.0f);
+		}
+	}
 	
 };
 
--- Script.cpp
+++ Script.cpp
@@ -1,23 +1,15 @@
 #include"Script.h"
 #include<filesystem>
-glm::vec2 getImageAspectRatio(const char* filename) {
-    int width, height, channels;
-    stbi_uc* imageData = stbi_load(filename, &width, &height, &channels, 0);
 
-    if (imageData != nullptr) {
-        float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
-        stbi_image_free(imageData);
-        return glm::vec2(aspectRatio, 1.0f);
-    }
-    else {
-        std::cout << "Failed to load image." << std::endl;
-        return glm::vec2(0.0f);
-    }
-}
+
 std::vector<Texture> textures;
 
 void Script::Start(Console& ovcon, InputSystem Input, PhysicsWorld& world, std::vector<Object>& sceneObjects, Camera& camera) {
+    std::string str = typeid(*this).name();
+    float fl = OV::PublicFloat(0, str);
 
+
+    
 
 
 }   
