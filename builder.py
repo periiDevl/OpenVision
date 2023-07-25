5fab3722eefa005053ebaf2ef9cd170da9a7400d
periiProggramerGuy <jonathan.peri56@gmail.com>
2023-07-25 10:10:57 +0300

    4.3 sec for rebuild

--- builder.py
+++ builder.py
@@ -6,6 +6,7 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import tkinter as tk
+import threading
 
 class HotReloadHandler(FileSystemEventHandler):
     def __init__(self, msbuild_path, sln_file_path, dst_directory_path):
@@ -13,6 +14,9 @@ class HotReloadHandler(FileSystemEventHandler):
         self.msbuild_path = msbuild_path
         self.sln_file_path = sln_file_path
         self.dst_directory_path = dst_directory_path
+        self.last_build_time = time.time()
+        self.file_timestamps = {}
+        self.build_lock = threading.Lock()
         self.build()
 
     def build(self):
@@ -21,6 +25,8 @@ class HotReloadHandler(FileSystemEventHandler):
         if result.returncode == 0:
             print("Build succeeded!")
             self.execute()
+            # Update last build time after a successful build
+            self.last_build_time = time.time()
         else:
             print("Build failed.")
 
@@ -40,36 +46,53 @@ class HotReloadHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.is_directory or not event.src_path.endswith('.cpp'):
             return
+
+        # Check if the file has been modified since the last build
+        if event.src_path in self.file_timestamps:
+            last_modified_time = self.file_timestamps[event.src_path]
+            current_time = time.time()
+            if current_time - last_modified_time < 1.0:  # Within 1 second, batch changes
+                return
+
         print(f"Detected change in {event.src_path}. Rebuilding...")
-        self.build()
+        self.file_timestamps[event.src_path] = time.time()  # Update the file's modified timestamp
+        self.schedule_build()
+
+    def schedule_build(self):
+        # Delay the build for 1 second to batch multiple changes together
+        time.sleep(0)
+        # Ensure only one thread can build at a time
+        with self.build_lock:
+            self.build()
+
 def get_data_from_file(file_path):
-  data = None
+    data = None
 
-  with open(file_path, "r") as f:
-    data = f.read()
+    with open(file_path, "r") as f:
+        data = f.read()
 
-  return data
+    return data
 
 def write_to_file():
-  text = entry_field.get()
+    text = entry_field.get()
 
-  if os.path.exists("MsbuildPATH.ov"):
-    with open("MsbuildPATH.txt", "w") as file:
-      file.truncate(0)
+    if os.path.exists("MsbuildPATH.ov"):
+        with open("MsbuildPATH.txt", "w") as file:
+            file.truncate(0)
 
-  file = open("MsbuildPATH.ov", "w")
+    file = open("MsbuildPATH.ov", "w")
 
-  file.write(text)
+    file.write(text)
 
-  file.close()
+    file.close()
+
 def check_if_file_exists(file_name):
+    if os.path.isfile(file_name):
+        return True
+    else:
+        return False
 
-  if os.path.isfile(file_name):
-    return True
-  else:
-    return False
 if __name__ == '__main__':
-    import tkinter as tk
     if (not check_if_file_exists(get_data_from_file("MsbuildPATH.ov"))):
         window = tk.Tk()
         lb = tk.Label(text="Please specify the MSBuild.exe path.")
@@ -100,7 +123,7 @@ if __name__ == '__main__':
 
     try:
         while True:
-            time.sleep(1)
+            time.sleep(0)
     except KeyboardInterrupt:
         observer.stop()
 
